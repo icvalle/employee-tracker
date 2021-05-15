a46c5209fd0135d8dfa7af0460782c866b2a0486
Iris Valle <iris.valle.design@gmail.com>
2021-05-14 20:55:34 -0400

    added view dept and view roles

--- db/db.js
+++ db/db.js
@@ -10,6 +10,19 @@ class DB {
             "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         );
     }
+
+    findAllDepartments() {
+        return this.connection.query(
+            "SELECT department.name FROM department"
+        );
+    }
+
+    findAllRoles() {
+        return this.connection.query(
+            "SELECT role.title, role.salary, role.department_id FROM role"
+        );
+    }
+
 }
 
 module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -1,11 +1,75 @@
 const inquirer = require("inquirer");
 const db = require("./db/db");
 
-// inquirer prompts
+const runSearch = () => {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees', 
+            'View All Departments', 
+            'View All Roles', 
+            'Add Employee', 
+            'Add Department', 
+            'Add Role', 
+            'Update Employee Role'
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View All Employees':
+            viewEmployees();
+            break;
+  
+          case 'View All Departments':
+            viewDept();
+            break;
+  
+          case 'View All Roles':
+            viewRoles();
+            break;
+  
+          case 'Add Employee':
+            addEmployee();
+            break;
+  
+          case 'Add Department':
+            addDept();
+            break;
+
+          case 'Add Role':
+            addRole();
+            break;
+        
+          case 'Update Employee Role':
+            updateEmployeeRole();
+            break;
+  
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+        }
+      });
+};
 
 async function viewEmployees () {
     let employees = await db.findAllEmployees();
     console.table(employees);
+    runSearch();
 }
 
-viewEmployees();
+async function viewDept () {
+  let departments = await db.findAllDepartments();
+  console.table(departments);
+  runSearch();
+}
+
+async function viewRoles () {
+  let roles = await db.findAllRoles();
+  console.table(roles);
+  runSearch();
+}
+
+runSearch();
